bd91ee044a70cc42f313928d0f32e49b5a116d73
Andreas Hoyer <a.hoyer@hello2morrow.de>
2015-09-07 14:03:31 +0200

    JENKINS-29187 Updated license header.

--- src/main/java/com/hello2morrow/sonargraph/jenkinsplugin/persistence/IReportReader.java
+++ src/main/java/com/hello2morrow/sonargraph/jenkinsplugin/persistence/IReportReader.java
@@ -1,6 +1,6 @@
-/*
- * Sonar Sonargraph Plugin
- * Copyright (C) 2009, 2010, 2011 hello2morrow GmbH
+/*******************************************************************************
+ * Jenkins Sonargraph Plugin
+ * Copyright (C) 2009-2015 hello2morrow GmbH
  * mailto: info AT hello2morrow DOT com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,9 +12,9 @@
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *******************************************************************************/
 package com.hello2morrow.sonargraph.jenkinsplugin.persistence;
 
 import java.io.IOException;
